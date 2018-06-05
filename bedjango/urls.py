a7e8cc0474ccb18f84bfba7dc1b99a15ba6c5d5a
Nima Karimi <Nima.karimi@live.com>
2018-06-05 23:06:32 +0430

    Fix | Media -ihope 3

--- bedjango/urls.py
+++ bedjango/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -22,9 +23,24 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+if settings.DEBUG==False:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+if settings.DEBUG==True:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+
+# if settings.DEBUG:
+#      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#
 
 if settings.DEBUG:
     import debug_toolbar
--- weblog/urls.py
+++ weblog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
-from django.views.static import serve
-from django.conf import settings
+
 
 from .views import PostListView , PostDetailView
 
@@ -12,18 +11,3 @@ urlpatterns = [
     path('<slug:slug>', PostDetailView.as_view(), name='post-detail')
 ]
 
-
-
-#
-# if settings.DEBUG==False:
-#     urlpatterns += [
-#         re_path(r'^media/(?P<path>.*)$', serve, {
-#             'document_root': settings.MEDIA_ROOT,
-#         }),
-#     ]
-# if settings.DEBUG==True:
-#     urlpatterns += [
-#         re_path(r'^media/(?P<path>.*)$', serve, {
-#             'document_root': settings.MEDIA_ROOT,
-#         }),
-#     ]
